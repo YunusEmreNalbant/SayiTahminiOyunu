e62ba69a7e2eb7837c1b96af5e25da20571b88bb
Yunus Emre Nalbant <nalbantyunusemre@hotmail.com>
2018-02-02 22:32:57 +0300

    Add project files.

---
+++ SayiTahminiOyunu/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SayiTahminiOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.LblGirilenTahmin = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtTahmin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 361);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(749, 60);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "KAZANABİLİR MİSİN?";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 305);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(749, 59);
+            this.progressBar1.TabIndex = 16;
+            // 
+            // LblGirilenTahmin
+            // 
+            this.LblGirilenTahmin.AutoSize = true;
+            this.LblGirilenTahmin.Location = new System.Drawing.Point(365, 248);
+            this.LblGirilenTahmin.Name = "LblGirilenTahmin";
+            this.LblGirilenTahmin.Size = new System.Drawing.Size(10, 13);
+            this.LblGirilenTahmin.TabIndex = 15;
+            this.LblGirilenTahmin.Text = " ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSalmon;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(502, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 36);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "TAHMİN ET";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(302, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 19);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "BİLGİ:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(239, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "OYUNU BAŞLAT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtTahmin
+            // 
+            this.TxtTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtTahmin.Location = new System.Drawing.Point(402, 171);
+            this.TxtTahmin.Multiline = true;
+            this.TxtTahmin.Name = "TxtTahmin";
+            this.TxtTahmin.Size = new System.Drawing.Size(94, 28);
+            this.TxtTahmin.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(127, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "0 ile 100 arasında bir sayı giriniz:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(692, 65);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sayı Tahmin Oyununa Hoş Geldiniz";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(750, 423);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.LblGirilenTahmin);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtTahmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label LblGirilenTahmin;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtTahmin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SayiTahminiOyunu/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SayiTahminiOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+        int deneme = 0;
+        Random gerceksayi = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            sayi = gerceksayi.Next(1, 101);
+          
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int girilensayi = Convert.ToInt32(TxtTahmin.Text);
+
+            if(sayi==girilensayi)
+            {
+                timer1.Stop();
+                MessageBox.Show("TEBRİKLER!" + deneme+ " .seferde bildiniz!");
+                
+
+
+            }
+            if(sayi<girilensayi)
+            {
+                LblGirilenTahmin.Text = "Daha küçük!";
+                deneme++;
+            }
+            if (sayi>girilensayi)
+            {
+                LblGirilenTahmin.Text = "Daha büyük!";
+                deneme++;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 5;
+            if(progressBar1.Value==5)
+            {
+                richTextBox1.Text = "SÜRE DOLUYOR !";
+            }
+            if(progressBar1.Value % 10==5)
+            {
+                richTextBox1.BackColor = Color.Red;
+            }
+            if(progressBar1.Value % 10 == 0)
+            {
+                richTextBox1.BackColor = Color.White;
+            }
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                richTextBox1.Text = " KAYBETTİN ! ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
